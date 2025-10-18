7458bd3bfe8dd23aa8440456455ec65fd433e9ad
versity1 <vgsicthub@gmail.com>
2025-10-18 15:35:18 +0100

    javascript added

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let username = prompt('Enter Username: ')
+username= username.trim()
+if(username === 'Ebuka'){
+    document.writeln(`welcome  ${username}`)
+}else{
+    document.writeln('Invalid username')
+}
+
