0e1df6106e9bde3d348edefe24f5ab2e1847cd00
versity1 <vgsicthub@gmail.com>
2025-10-18 15:39:22 +0100

    added password login

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let username = prompt('Enter Username: ')
+let password = prompt('Enter password: ')
 username= username.trim()
-if(username === 'Ebuka'){
+if(username === 'Ebuka' & password === '12345678'){
     document.writeln(`welcome  ${username}`)
 }else{
     document.writeln('Invalid username')
